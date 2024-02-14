3d5dab3bcb182253f655c51b17eedeaaacde85ab
Manu <manuellafflopes@gmail.com>
2024-02-13 21:50:10 -0700

    bonus exercise

---
+++ scriptJQuery.js
@@ -0,0 +1,38 @@
+const photos = [
+    'https://images.unsplash.com/photo-1682687219640-b3f11f4b7234?q=80&w=1024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+
+    'https://images.unsplash.com/photo-1706463629335-d92264bbfd6f?q=80&w=1024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+
+    'https://images.unsplash.com/photo-1682695796497-31a44224d6d6?q=80&w=1024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+]
+
+const img = $('img')
+console.log(img)
+let position = 0
+
+img.attr('src', photos[position])
+
+const previousButton = $('.previousButton')
+previousButton.click(previousImage)
+
+const nextButton = $('.nextButton')
+
+nextButton.click(nextImage)
+
+function nextImage() {
+    if (position == photos.length - 1) {
+        position = 0
+    } else {
+        position++
+    }
+    img.attr('src', photos[position])
+}
+
+function previousImage() {
+    if (position == 0) {
+        position = photos.length - 1
+    } else {
+        position--
+    }
+    img.attr('src', photos[position])
+}
